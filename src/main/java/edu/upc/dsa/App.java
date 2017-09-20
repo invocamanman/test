947ff33176aa303660304cb52b6f3f9258435cdb
invocamanman <jesus.ligero@yahoo.es>
2017-09-20 13:33:50 +0200

    Codigo java

---
+++ src/main/java/edu/upc/dsa/App.java
@@ -0,0 +1,18 @@
+package edu.upc.dsa;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static void f() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        int x = 1;
+        f();
+        x=x+1;
+        System.out.println("Bye bye "+x);
+    }
+}
